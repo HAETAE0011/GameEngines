f7451d41f466efc10693dff755c38e0f503e44cd
HAETAE0011 <john0011@naver.com>
2021-05-14 16:16:25 -0400

    external libraries

--- HAETAEGameEngine_V01/HAETAEGameEngine_V01/main.cpp
+++ HAETAEGameEngine_V01/HAETAEGameEngine_V01/main.cpp
@@ -10,36 +10,36 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-	//XML
-	cout << "Capstone group" << endl;
-	xml_document<> doc;
-	xml_node<>* root;
-	// Read the xml file into a vector
-	ifstream file("./Resource/Test.xml");
+	////XML
+	//cout << "Capstone group" << endl;
+	//xml_document<> doc;
+	//xml_node<>* root;
+	//// Read the xml file into a vector
+	//ifstream file("./Resource/Test.xml");
 
-	vector<char> buffer((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-	buffer.push_back('\0');
+	//vector<char> buffer((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+	//buffer.push_back('\0');
 
-	// Parse the buffer using the xml file parsing library into doc 
-	doc.parse<0>(&buffer[0]);
+	//// Parse the buffer using the xml file parsing library into doc 
+	//doc.parse<0>(&buffer[0]);
 
-	//close file here
-	file.close();
+	////close file here
+	//file.close();
 
-	// Find our root node
-	root = doc.first_node("ProjectManAtArms");
-	// Iterate over the members
-	for (xml_node<>* memeberNode = root->first_node("Member"); memeberNode; memeberNode = memeberNode->next_sibling())
-	{
-		printf("member: %s living in %s. ", memeberNode->first_attribute("name")->value(), memeberNode->first_attribute("location")->value());
+	//// Find our root node
+	//root = doc.first_node("ProjectManAtArms");
+	//// Iterate over the members
+	//for (xml_node<>* memeberNode = root->first_node("Member"); memeberNode; memeberNode = memeberNode->next_sibling())
+	//{
+	//	printf("member: %s living in %s. ", memeberNode->first_attribute("name")->value(), memeberNode->first_attribute("location")->value());
 
-		//the jobs
-		xml_node<>* job_node = memeberNode->first_node("role");
-		printf("for this project, I am a %s, %s. ",	job_node->first_attribute("job")->value(), job_node->first_attribute("description")->value());
-		printf(job_node->value());
-		
-		cout << endl;
-	}
+	//	//the jobs
+	//	xml_node<>* job_node = memeberNode->first_node("role");
+	//	printf("for this project, I am a %s, %s. ",	job_node->first_attribute("job")->value(), job_node->first_attribute("description")->value());
+	//	printf(job_node->value());
+	//	
+	//	cout << endl;
+	//}
 
 	CoreEngine::GetInstace()->SetGameInterface(new Game2, Renderer::RENDERER_TYPE::OPENGL);
 
